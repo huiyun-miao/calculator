ba222683298a168b65a85f805a257de3eb7d6b70
Huiyun Miao <109335165+huiyun-miao@users.noreply.github.com>
2026-02-13 20:30:22 +0200

    Add basic functions to display and add numbers

--- app.js
+++ app.js
@@ -27,3 +27,48 @@ function operate(num1, num2, operator) {
       divide(num1, num2);
   }
 }
+
+let num1, num2, operator;
+const display = document.querySelector("#display");
+
+const numberBtns = Array.from(document.querySelectorAll("button")).filter((b) =>
+  Number.isInteger(Number(b.id)),
+);
+
+const operatorBtns = Array.from(document.querySelectorAll("button")).filter(
+  (b) => !Number.isInteger(Number(b.id)),
+);
+
+const addBtn = document.querySelector("#add");
+const subtractBtn = document.querySelector("#subtract");
+const multiplyBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+const equalBtn = document.querySelector("#equal");
+
+numberBtns.forEach((b) => {
+  b.addEventListener("click", () => {
+    if (operator === undefined) {
+      num1 = updateInput(num1, Number(b.id));
+      display.value = num1;
+    } else {
+      num2 = updateInput(num2, Number(b.id));
+      display.value = `${num1}${operator}${num2}`;
+    }
+  });
+});
+
+addBtn.addEventListener("click", () => {
+  if (!operator) {
+    operator = "+";
+    display.value += operator;
+  } else {
+    display.value = `${add(num1, num2)}+`;
+    num1 = add(num1, num2);
+    num2 = undefined;
+  }
+});
+
+function updateInput(number, input) {
+  if (number === undefined) return input;
+  return number * 10 + input;
+}
